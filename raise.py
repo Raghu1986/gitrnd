e56c1f9ae746dfd378f778a2fde5731c6929e422
Raghu <Raghu1986@outlook.com>
2023-03-27 00:47:20 +0530

    Fixed theanother error line

--- raise.py
+++ raise.py
@@ -1,3 +1,3 @@
 print("Rise and Shine")
 print("Lets's get it started")
-print("Added another line for testing pull request in working branch"
+print("Added another line for testing pull request in working branch")
